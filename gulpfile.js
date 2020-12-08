2ac3f6fb4c42b01b44e9616697b6d96707b3de47
Bishop <bishop1.elaigwu@yahoo.com>
2020-12-08 20:21:02 +0100

    Development set up

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var plumber = require("gulp-plumber");
 var browserSync = require("browser-sync");
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task("sass", function () {
+gulp.task("sass", async function () {
   gulp
     .src(["node_modules/bootstrap/scss/bootstrap.scss", "scss/main.scss"])
     .pipe(plumber())
@@ -20,19 +20,19 @@ gulp.task("sass", function () {
 });
 
 // Move the javascript files into our /src/js folder
-gulp.task("js", function () {
+gulp.task("js", async function () {
   return gulp
     .src([
       "node_modules/bootstrap/dist/js/bootstrap.min.js",
       "node_modules/jquery/dist/jquery.min.js",
-      "node_modules/tether/dist/js/tether.min.js",
+      "node_modules/popper.js/dist/popper.min.js",
     ])
     .pipe(gulp.dest("js"))
     .pipe(browserSync.stream());
 });
 
 // Static Server + watching scss/html files
-gulp.task("browser-sync", function () {
+gulp.task("browser-sync", async function () {
   browserSync.init(["css/*.css", "js/*.js"], {
     server: {
       baseDir: "./",
@@ -40,7 +40,7 @@ gulp.task("browser-sync", function () {
   });
 });
 
-gulp.task("default", ["sass", "js", "browser-sync"], function () {
-  gulp.watch("scss/**/*.scss", ["sass"]);
+gulp.task("default", gulp.series("sass", "js", "browser-sync", async function () {
+  gulp.watch("scss/**/*.scss", gulp.series("sass"));
   gulp.watch("*.html").on("change", browserSync.reload);
-});
+}));
